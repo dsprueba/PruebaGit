eceeab51bc8f456bc5e73208b6105ebdbf5be2f2
Programador <Programador@DESARROLLO-3>
2016-09-22 16:55:40 -0400

    Mi primer commit nº 1

---
+++ MiprimerGit/src/PrimeraPrueba.java
@@ -0,0 +1,13 @@
+
+public class PrimeraPrueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Probando Git");
+
+	}
+
+}
